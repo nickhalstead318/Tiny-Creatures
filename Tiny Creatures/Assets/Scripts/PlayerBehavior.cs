bef2b5ce1f34845cc09bc4994f902994bf5b5384
nickhalstead318 <nickhalstead2001@yahoo.com>
2024-10-04 19:38:42 -0500

    Delete bad movement logic
    Player will accelerate and slowdown slightly upon key press and key release

---
+++ Tiny Creatures/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _playerSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Starting position
+        transform.position = new Vector3(0, 0, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDir = Input.GetAxis("Horizontal");
+
+        float yDir = Input.GetAxis("Vertical");
+        
+
+        transform.position += (xDir * Vector3.right + yDir * Vector3.up) * _playerSpeed * Time.deltaTime;
+    }
+}
